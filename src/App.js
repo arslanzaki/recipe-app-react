65235d32dcc12db5c1e2f639932292286ad5aa99
Arslan Zaki <59029560+arslanzaki@users.noreply.github.com>
2022-08-12 10:37:38 +0500

    Basic Routing & Cuisines Setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Pages from "./pages/Pages";
+import Category from "./components/Category";
+import {BrowserRouter} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+      <Category />
       <Pages />
-    </div>
+    </BrowserRouter>
   );
 }
 
