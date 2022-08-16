4576398aef444c650edd3d8a43f1a865210d9486
Arslan Zaki <59029560+arslanzaki@users.noreply.github.com>
2022-08-16 11:48:33 +0500

    Final Touches

--- src/App.js
+++ src/App.js
@@ -2,14 +2,45 @@ import Pages from "./pages/Pages";
 import Category from "./components/Category";
 import { BrowserRouter } from "react-router-dom";
 import Search from "./components/Search";
+import styled from "styled-components";
+import { GiKnifeFork } from "react-icons/gi";
+import { Link } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
+      <Nav>
+        <GiKnifeFork />
+        <Logo to={"/"}>RecipeApp</Logo>
+      </Nav>
       <Search />
       <Category />
       <Pages />
     </BrowserRouter>
   );
 }
+
+const Logo = styled(Link)`
+  @font-face {
+    font-family: "nordeco_regular";
+    src: url("nordeco-regular-webfont.woff2") format("woff2"),
+      url("nordeco-regular-webfont.woff") format("woff");
+    font-style: normal;
+  }
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: bold;
+  font-family: "Nordeco";
+`;
+
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg {
+    font-size: 2rem;
+    margin-right: 1rem;
+  }
+`;
 
 export default App;
--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/splide/dist/css/splide.min.css";
+import { Link } from "react-router-dom";
 
 function Popular() {
   const [popular, setPopular] = useState([]);
@@ -20,7 +21,7 @@ function Popular() {
       setPopular(data.recipes);
       console.log(data.recipes);
     } else {
-      console.log("Parsed")
+      console.log("Parsed");
       setPopular(JSON.parse(check));
     }
   };
@@ -40,9 +41,11 @@ function Popular() {
           return (
             <SplideSlide key={recipe.id}>
               <Card>
-                <p>{recipe.title}</p>
-                <img src={recipe.image} alt={recipe.title} />
-                <Gradient />
+                <Link to={"/recipe/" + recipe.id}>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title} />
+                  <Gradient />
+                </Link>
               </Card>
             </SplideSlide>
           );
